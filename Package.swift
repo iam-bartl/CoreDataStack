288a02d12dfa1603187b45b92daeb0f894854a03
Yauhen Rusanau <yauhen.rusanau@apalon.com>
2024-02-08 11:31:16 +0200

    Update Identifier dependency

--- Package.swift
+++ Package.swift
@@ -4,17 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "Utils",
+    name: "CoreDataStack",
     platforms: [.iOS("16.4"), .macOS("13.3")],
     products: [
         .library(name: "CoreDataStack", targets: ["CoreDataStack"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/iam-bartl/Identifier.git", branch: "main")
+        .package(url: "git@github.com:iam-bartl/SwiftToolbox.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "CoreDataStack", dependencies: ["Identifier"]),
+        .target(name: "CoreDataStack", dependencies: [
+            .product(name: "Identifier", package: "SwiftToolbox")
+        ]),
     ]
 )
